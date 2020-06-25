585474ca480a15a2f6fa3cda4177bbd225c6ef52
MarcosJLR <socram.693@gmail.com>
2020-06-25 08:35:00 -0400

    Network to classify handwritten digits with perceptron neurons

---
+++ src/DigitPerceptronNetwork.cpp
@@ -0,0 +1,55 @@
+#include "DigitPerceptronNetwork.hpp"
+
+namespace artnn
+{
+    double DigitPerceptronNet::randomInitializer()
+    {
+        static std::random_device rd;
+        static std::mt19937 e2(rd());
+        static std::uniform_real_distribution<double> dist(-0.05, 0.05);
+
+        return dist(e2);
+    }
+
+    double DigitPerceptronNet::train(const std::vector<double>& X, int d)
+    {
+        double acumError = 0.0;
+        for(int i = 0; i < 10; i++)
+        {
+            mNeurons[i]->train(X, i == d ? 1.0 : 0.0);
+
+            double y = mNeurons[i]->evaluate(X);
+            double dd = i == d ? 1.0 : 0.0;
+            double sqError = (dd - y) * (dd - y);
+
+            acumError += sqError;
+        }
+        return acumError / 10.0;
+    }
+
+    double DigitPerceptronNet::trainEpoch(std::vector<std::pair<std::vector<double>, int>>& input)
+    {
+        // Randomize input
+        std::random_shuffle(input.begin(), input.end());
+
+        double acumError = 0.0; 
+        for(auto& [X, d] : input)
+        {
+            acumError += train(X, d);
+        }
+
+        return acumError / (double) input.size();
+    }
+
+    int DigitPerceptronNet::trainFull(std::vector<std::pair<std::vector<double>, int>>& input,
+                                   const uint maxEpoch, const double eps)
+    {
+        for(uint i = 0; i < maxEpoch; i++)
+        {
+            if(trainEpoch(input) <= eps) return i+1;
+        }
+
+        return maxEpoch;
+    }
+
+};
