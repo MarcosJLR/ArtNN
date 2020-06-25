479edc6787fec4544564567ffa3e4f4164be7808
MarcosJLR <socram.693@gmail.com>
2020-06-25 09:07:13 -0400

    Class to read CSV files

---
+++ src/CSVReader.cpp
@@ -0,0 +1,53 @@
+#include "CSVReader.hpp"
+
+namespace artnn
+{
+    std::vector<std::vector<std::string>> CSVReader::getData()
+    {
+        std::ifstream file(filename);
+
+        std::vector<std::vector<std::string>> dataList;
+        std::string line = "";
+
+        while(getline(file, line))
+        {
+            dataList.push_back(parseLine(line));
+        }
+
+        file.close();
+
+        return dataList;
+    }
+
+    std::vector<std::string> CSVReader::parseLine(std::string& line)
+    {
+        std::vector<std::string> parsed;
+        std::string currentValue = "";
+
+        for(char c : line)
+        {
+            if(!isDelimiter(c))
+            {
+                currentValue += c;
+            }
+            else if(!currentValue.empty())
+            {
+                parsed.push_back(currentValue);
+                currentValue.clear();
+            }
+        }
+
+        if(!currentValue.empty())
+        {
+            parsed.push_back(currentValue);
+            currentValue.clear();
+        }
+
+        return parsed;
+    }
+
+    bool CSVReader::isDelimiter(char c)
+    {
+        return delimiter.find(c) != delimiter.end();
+    }
+};
---
+++ src/CSVReader.hpp
@@ -0,0 +1,35 @@
+/*
+ * CSV reader
+ * 
+ * Class to abstract reading from a csv file
+ *
+ * Author: Marcos Lerones
+ */
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace artnn
+{
+    class CSVReader
+    {
+    public:
+        CSVReader(std::string filename, std::string delim = ",")
+            : filename(filename), delimiter(delim) {}
+
+        // Get the contents of the file
+        std::vector<std::vetor<std::string>> getData();
+
+    private:
+        std::string filename;
+        std::string delimiter;
+
+        // Helper Functions
+        std::vector<std::string> parseLine(std::string& line);
+        bool isDelimiter(char c);
+    }
+};
