8bfc7b986cc15e1d806de94f1e65f5ce33560a23
MarcosJLR <socram.693@gmail.com>
2020-07-06 08:34:35 -0400

    Fixed some bugs

--- src/DigitMLPNetwork.cpp
+++ src/DigitMLPNetwork.cpp
@@ -1,5 +1,7 @@
 #include "DigitMLPNetwork.hpp"
 
+#include <iostream>
+
 namespace artnn
 {
     int DigitMLPNet::evaluate(const std::vector<double>& X)
@@ -24,6 +26,23 @@ namespace artnn
         return answer;
     }
 
+    int DigitMLPNet::evaluateAux(const std::vector<double>& X)
+    {
+        std::vector<double> Y = network->evaluate(X);
+
+        int answer = -1;
+        for(int i = 0; i < 10; i++)
+        {
+            if(Y[i] > 0.5)
+            {
+                if(answer == -1) { answer = i; }
+                else { answer = -1; break; }
+            }
+        }
+
+        return answer;
+    }
+
     std::vector<double> DigitMLPNet::digitToVector(int d)
     {
         std::vector<double> v(10, 0.0);
@@ -33,29 +52,43 @@ namespace artnn
     }
 
     int DigitMLPNet::trainFull(const std::vector<std::pair<std::vector<double>, int>>& rawData,
+                               const std::vector<std::pair<std::vector<double>, int>>& rawValidData,
                                const uint maxEpochs, const double epsilon,
-                               const std::string logFileName)
+                               const std::string trainLogFileName,
+                               const std::string validLogFileName)
     {
         std::vector<std::pair<std::vector<double>, std::vector<double>>> trainingData(rawData.size());
-        std::ofstream logFile(logFileName);
+        std::vector<std::pair<std::vector<double>, std::vector<double>>> validationData(rawValidData.size());
+        std::ofstream trainLog(trainLogFileName);
+        std::ofstream validLog(validLogFileName);
+        int totEpochs = maxEpochs;
 
         for(uint i = 0; i < rawData.size(); i++)
         {
             auto& [X, d] = rawData[i];
             trainingData[i] = { X, digitToVector(d) };
         }
+        for(uint i = 0; i < rawValidData.size(); i++)
+        {
+            auto& [X, d] = rawValidData[i];
+            validationData[i] = { X, digitToVector(d) };
+        }
 
         for(uint i = 0; i < maxEpochs; i++)
         {
-            double avgError = network->trainEpoch(trainingData);
-            logFile << i << "," << avgError << std::endl;
+            double avgTrainError = network->trainEpoch(trainingData);
+            trainLog << i << "," << avgTrainError << std::endl;
 
-            if(avgError < epsilon) { return i; }
+            double avgValidationError = network->validateEpoch(validationData);
+            validLog << i << "," << avgValidationError << std::endl;
+
+            if(avgTrainError < epsilon) { totEpochs = i; break; }
         }
 
-        logFile.close();
+        trainLog.close();
+        validLog.close();
 
-        return maxEpochs;
+        return totEpochs;
     }
 
     int DigitMLPNet::test(const std::vector<std::pair<std::vector<double>, int>>& testingData)
@@ -68,5 +101,26 @@ namespace artnn
         }
 
         return answer;
+    }
+
+    int DigitMLPNet::testAux(const std::vector<std::pair<std::vector<double>, int>>& testingData)
+    {
+        int answer = 0;
+        for(auto& [X, d] : testingData)
+        {
+            int y = evaluateAux(X);
+            if(y == d) { answer++; }
+        }
+
+        return answer;
+    }
+
+    double DigitMLPNet::randomInitializer()
+    {
+        static std::random_device rd;
+        static std::mt19937 e2(rd());
+        static std::uniform_real_distribution<double> dist(-0.05, 0.05);
+
+        return dist(e2);
     }
 };
--- src/DigitMLPNetwork.hpp
+++ src/DigitMLPNetwork.hpp
@@ -22,20 +22,30 @@ namespace artnn
         {
             std::vector<uint> sizes = { 784, hiddenNodes, 10 };
             network = new MLP<double>(sizes, tEtha, tAlpha);
+            network->randomizeWeights(randomInitializer);
         }
 
         // Classify the given image
         // Returns -1 if it didn't recognize any digit
         int evaluate(const std::vector<double>& X);
 
+        // Same as evaluate but different criteria
+        int evaluateAux(const std::vector<double>& X);
+
         // Train for a specified number of epochs
         // or until error is less than some epsilon
         // Return number of epochs trained
         int trainFull(const std::vector<std::pair<std::vector<double>, int>>& trainingData,
+                      const std::vector<std::pair<std::vector<double>, int>>& validationData,
                       const uint maxEpochs = 50, const double epsilon = 0.0,
-                      const std::string logFileName = "log.csv");
+                      const std::string trainLogFileName = "trainError.csv",
+                      const std::string validLogFileName = "validError.csv");
 
+        // Count the number of correctly classified digits
         int test(const std::vector<std::pair<std::vector<double>, int>>& testingData);
+
+        // Count the number of correctly classified digits
+        int testAux(const std::vector<std::pair<std::vector<double>, int>>& testingData);
 
     private:
         MLP<double>* network;
@@ -43,5 +53,8 @@ namespace artnn
         // Transform digit into canonical vector of size 10
         // with 1 on the digit index and 0 everywhere else
         std::vector<double> digitToVector(int d);
+
+        // Helper function to init weights
+        static double randomInitializer();  
     };
 };
